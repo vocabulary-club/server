f8a7cdd2e90bbd3362f56c87e145e06fe93283bd
lkhaajav <lkhaching@gmail.com>
2026-02-08 21:05:07 +0900

    keep doing

--- src/main/java/web/server/api/config/SecurityConfig.java
+++ src/main/java/web/server/api/config/SecurityConfig.java
@@ -107,7 +107,7 @@ public class SecurityConfig {
         );
 
         http.authorizeHttpRequests((auth) -> auth
-                .requestMatchers("/token/renew").permitAll()
+                .requestMatchers("/token/renew", "/privacy-policy", "/term-of-service").permitAll()
                 .anyRequest().authenticated());
 
         http.addFilterAfter(new MyJwtFilter(jwtUtil), UsernamePasswordAuthenticationFilter.class);
